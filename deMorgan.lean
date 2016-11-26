7d3f62c4056db1f4902c7f277c5110b32fe5c894
Adam Kurkiewicz <adam@kurkiewicz.pl>
2016-11-26 14:08:11 +0000

    proof of deMorgan

--- deMorgan.lean
+++ deMorgan.lean
@@ -1,29 +1,82 @@
--- this is a cheat, useful for testing. It allows us to
--- prove ANY proposition, by inhabiting that proposition's type.
+-- Defining this axiom is cheating,
+-- as it allows us to prove any Prop,
+-- giving rise to a complete, but inconsistent logic.
+-- However, *using* this axiom may or may not be cheating.
+-- Using the axiom is equivalent to a mathematician saying
+-- "it is easy to see that".
+-- We will use this axiom to postulate a lemma without proof.
+-- We can comment it out when we give proofs of all the lemmas.
 
-constant always : Π (p : Prop), p
+-- this allows us to prove anything!
+-- axiom always : Π (p : Prop), p
 
-open classical
 
+-- `variable` is an unfortunate name, conflicting
+-- with usual meaning of the word.
+-- `p` and `q` are *implicit parameters*
+-- passed to our functions. (All lemmas and theorems
+-- will be functions)
 variables p q : Prop
 
 
-lemma deMorganHelper : ¬(p ∧ q) → p → q → false :=
+-- These introduction and elimination rules
+-- are analogous to and.intro and and.elim,
+-- but they work with falsehoods as opposed to truths.
+
+-- `lemma` is syntactic sugar for `definition`, 
+lemma andFalseElimRight : ¬(p ∧ q) → (p → ¬q) :=
+    -- `assume` is syntactic sugar for `λ`
     assume (LHS : ¬(p ∧ q)) (Hp: p),
     (λ (Hq: q), LHS (and.intro Hp Hq))
 
+
+lemma andFalseElimLeft : ¬(p ∧ q) → (q → ¬p) :=
+    assume (LHS : ¬(p ∧ q)) (Hq: q),
+    (λ (Hp: p), LHS (and.intro Hp Hq))
+
+
+lemma andFalseIntroLeft : ¬p → ¬(p ∧ q) :=
+    assume (LHS : ¬p),
+    (λ Hnpq: (p ∧ q), LHS (and.elim_left Hnpq))
+
+
+lemma andFalseIntroRight : ¬p → ¬(q ∧ p) :=
+    assume (LHS : ¬p),
+    (λ Hnpq: (q ∧ p), LHS (and.elim_right Hnpq))
+
+
+-- We need classical logic for excluded middle to prove de Morgan's.
+open classical
+
+
+-- Prove de Morgan's.
 lemma deMorganAndLeft : ¬(p ∧ q) → (¬p ∨ ¬q) :=
   assume LHS : ¬(p ∧ q),
-  or.elim (em p)
-    (λ Hp: p, or.intro_right (¬p) (deMorganHelper p q LHS Hp))
+  -- The claim easily follows from excluded middle on p 
+  or.elim (em p) -- (p ∨ ¬p)
+    -- if p, we have to first show that ¬q, and then introduce desired disjunction (¬p ∨ ¬q)
+    (λ Hp: p, or.intro_right (¬p) (andFalseElimRight p q LHS Hp))
+    -- if ¬p, we can simply introduce desired disjunction (¬p ∨ ¬q)
     (λ Hp: (¬p), or.intro_left (¬q) Hp)
+
 
 lemma deMorganAndRight : (¬p ∨ ¬q) → ¬(p ∧ q) :=
   assume LHS : (¬p ∨ ¬q),
-  always (¬(p ∧ q))
+  or.elim LHS (andFalseIntroLeft p q) (andFalseIntroRight q p)
 
-lemma deMorganAnd : ¬(p ∧ q) ↔ (¬p ∨ ¬q) :=
+
+-- If this line type-checks correctly, then the theorem is proven.
+-- In order to be completely certain about the proof,
+-- one has to additionally check that:
+-- 1. No additional `axioms` have been defined.
+--    Some axioms, like my `always` axiom can lead to
+--    inconsistent foundations.
+-- 2. The statement of the theorem is correct, i.e.
+--    in our case deMorganAnd really *does* correspond
+--    to one of the theorems commonly referred to as de Morgan's laws.
+theorem deMorganAnd (p : Prop) (q: Prop) : ¬(p ∧ q) ↔ (¬p ∨ ¬q) :=
   iff.intro (deMorganAndLeft p q) (deMorganAndRight p q)
 
-check deMorganAnd
 
+-- prints the type of deMorganAnd.
+check deMorganAnd
