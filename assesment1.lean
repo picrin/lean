b0687991678afca7556935612bd2709a22eb955a
Adam Kurkiewicz <adam@kurkiewicz.pl>
2016-12-04 19:10:41 +0000

    finished step 2

--- assesment1.lean
+++ assesment1.lean
@@ -18,6 +18,7 @@ lemma andFalseIntroRight : ¬p → ¬(q ∧ p) :=
     assume (LHS : ¬p),
     (λ Hnpq: (q ∧ p), LHS (and.elim_right Hnpq))
 
+
 lemma deMorganAndLeft : ¬(p ∧ q) → (¬p ∨ ¬q) :=
     assume LHS : ¬(p ∧ q),
     -- The claim easily follows from excluded middle on p 
@@ -30,6 +31,10 @@ lemma deMorganAndLeft : ¬(p ∧ q) → (¬p ∨ ¬q) :=
 lemma deMorganAndRight : (¬p ∨ ¬q) → ¬(p ∧ q) :=
     assume LHS : (¬p ∨ ¬q),
     or.elim LHS (andFalseIntroLeft p q) (andFalseIntroRight q p)
+
+check (assume (Hnp : (¬p)), deMorganAndRight p (¬q) (or.intro_left (¬¬q) Hnp))
+
+check λ (Hp : p), or.intro_left q (Hp : p) 
 
 theorem deMorganAnd : ¬(p ∧ q) ↔ (¬p ∨ ¬q) :=
     iff.intro (deMorganAndLeft p q) (deMorganAndRight p q)
@@ -64,6 +69,14 @@ theorem doubleNegRev : p → ¬¬p :=
             (assume Hnp : ¬p, absurd Hp Hnp)
             (assume Hnnp : ¬¬p, Hnnp)
 
+lemma deMorganAndRightNot : (¬p ∨ q) → ¬(p ∧ ¬q) :=
+    assume LHS : (¬p ∨ q),
+    or.elim LHS
+        (assume (Hnp : (¬p)), deMorganAndRight p (¬q) (or.intro_left (¬¬q) Hnp))
+        (assume (Hq : q), deMorganAndRight p (¬q) (or.intro_right (¬p) (doubleNegRev q Hq)))
+
+
+
 theorem deMorganContra : ¬(¬p ∧ ¬q) → ¬¬(p ∨ q) :=
     λ (LHS: ¬(¬p ∧ ¬q)), doubleNegRev (p ∨ q) (or.elim ((deMorganAndLeft ¬p ¬q) LHS)
         (λ (H1: ¬¬p), or.intro_left q (doubleNeg p H1))
@@ -73,7 +86,7 @@ theorem deMorganOrLeft : ¬(p ∨ q) → (¬p ∧ ¬q) :=
     contrapositionRev (¬(p ∨ q)) (¬p ∧ ¬q) (deMorganContra p q)
 
 
-theorem equiTrans : (p ↔ q) → (q ↔ r) → (p ↔ r) :=
+theorem equiTrans {p q r : Prop} : (p ↔ q) → (q ↔ r) → (p ↔ r) :=
     assume eq1 : (p ↔ q),
         assume eq2 : (q ↔ r),
             iff.intro
@@ -92,7 +105,7 @@ theorem distributivity : p ∧ (q ∨ r) → (p ∧ q) ∨ (p ∧ r) :=
             (p ∧ q)
             (and.intro (and.elim_left LHS) Hr))
 
-lemma s1 : ¬(p ∨ (q ∧ ¬r)) ∧ q ↔ (¬p ∧ ¬(q ∧ ¬r)) ∧ q :=
+lemma s1 {p q r : Prop} : ¬(p ∨ (q ∧ ¬r)) ∧ q ↔ (¬p ∧ ¬(q ∧ ¬r)) ∧ q :=
     iff.intro
         (assume (LHS : ¬(p ∨ (q ∧ ¬r)) ∧ q),
             and.intro
@@ -116,14 +129,76 @@ lemma doubleNegOrRev : (q ∨ ¬¬r) → (q ∨ r) :=
         (λ (Hr : ¬¬r), or.intro_right q (doubleNeg r Hr))
 
 
-lemma s2 : (¬p ∧ ¬(q ∧ ¬r)) ∧ q → (¬p ∧ (¬q ∨ r)) ∧ q :=
-    assume (LHS : (¬p ∧ ¬(q ∧ ¬r)) ∧ q),
+lemma s2 {p q r : Prop} : (¬p ∧ ¬(q ∧ ¬r)) ∧ q ↔ (¬p ∧ (¬q ∨ r)) ∧ q :=
+    iff.intro 
+    (assume (LHS : (¬p ∧ ¬(q ∧ ¬r)) ∧ q),
         and.intro
-        (and.intro (and.elim_left (and.elim_left LHS))
-        (doubleNegOrRev (¬q) r (deMorganAndLeft q (¬r) (and.elim_right (and.elim_left LHS)))))
-        (and.elim_right LHS)
+        (and.intro
+            (and.elim_left (and.elim_left LHS))
+            (doubleNegOrRev (¬q) r (deMorganAndLeft q (¬r) (and.elim_right (and.elim_left LHS)))))
+        (and.elim_right LHS))
+    (assume (LHS2 : (¬p ∧ (¬q ∨ r)) ∧ q),
+        and.intro
+        ((and.intro
+            ((and.elim_left (and.elim_left LHS2)) : (¬p))
+            ((deMorganAndRightNot (q) (r) ((and.elim_right (and.elim_left (LHS2 : (¬p ∧ (¬q ∨ r)) ∧ q))) : (¬q ∨ r))) : (¬(q ∧ ¬r)))) : (¬p ∧ ¬(q ∧ ¬r)))
+        ((and.elim_right (LHS2 : (¬p ∧ (¬q ∨ r)) ∧ q)) : q))
 
-            
+lemma s3  {p q r : Prop} : (¬p ∧ (¬q ∨ r)) ∧ q ↔ ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q := sorry
+
+lemma s4  {p q r : Prop} : ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q ↔ ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q := sorry
+
+lemma s5  {p q r : Prop} : ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ (((¬p ∨ ¬p) ∧ (¬p ∨ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q := sorry
+
+lemma s6  {p q r : Prop} : (((¬p ∨ ¬p) ∧ (¬p ∨ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ ((¬p ∨ r) ∧ (¬q ∨ ¬p) ∧ (¬q ∨ r)) ∧ q  := sorry
+
+lemma s7  {p q r : Prop} : ((¬p ∨ r) ∧ (¬q ∨ ¬p) ∧ (¬q ∨ r)) ∧ q ↔ (¬p ∨ r) ∧ (¬q ∨ r) ∧ (¬q ∨ ¬p) ∧ q  := sorry
+
+lemma s8  {p q r : Prop} : (¬p ∨ r) ∧ (¬q ∨ r) ∧ (¬q ∨ ¬p) ∧ q ↔ (¬(p ∨ q) ∨ r) ∧ ((¬q ∧ q) ∨ (¬p ∧ q)) := sorry
+
+lemma s9  {p q r : Prop} : (¬(p ∨ q) ∨ r) ∧ ((¬q ∧ q) ∨ (¬p ∧ q)) ↔ (¬(p ∨ q) ∨ r) ∧ (¬p ∧ q) := sorry
+
+lemma s11 {p q r : Prop} : (¬(p ∨ q) ∨ r) ∧ (¬p ∧ q) ↔ (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q)) := sorry
+
+lemma s12 {p q r : Prop} : (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q)) ↔ (r ∧ (¬p ∧ q)) ∨ (¬p ∧ ¬q ∧ ¬p ∧ q) := sorry
+
+lemma s13 {p q r : Prop} : (r ∧ (¬p ∧ q)) ∨ (¬p ∧ ¬q ∧ ¬p ∧ q) ↔ ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q := sorry
+
+lemma s14 {p q r : Prop} : ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q ↔ ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q := sorry
+
+lemma s15 {p q r : Prop} : ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ ((¬p ∧ q) ∧ r) := sorry
+
+theorem assessedExercice1 : ¬(p ∨ (q ∧ ¬r)) ∧ q ↔ ((¬p ∧ q) ∧ r) :=
+    (equiTrans
+        (equiTrans
+            (equiTrans
+                (equiTrans
+                    (equiTrans
+                        (equiTrans
+                            (equiTrans
+                                (equiTrans
+                                    (equiTrans
+                                        (equiTrans
+                                            (equiTrans
+                                                (equiTrans
+                                                    (equiTrans
+                                                        (s1 : ¬(p ∨ (q ∧ ¬r)) ∧ q ↔ (¬p ∧ ¬(q ∧ ¬r)) ∧ q)
+                                                        (s2 : (¬p ∧ ¬(q ∧ ¬r)) ∧ q ↔ (¬p ∧ (¬q ∨ r)) ∧ q))
+                                                (s3 : (¬p ∧ (¬q ∨ r)) ∧ q ↔ ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q))
+                                            (s4 : ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q ↔ ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q))
+                                        (s5 : ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ (((¬p ∨ ¬p) ∧ (¬p ∨ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q))
+                                    (s6 : (((¬p ∨ ¬p) ∧ (¬p ∨ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ ((¬p ∨ r) ∧ (¬q ∨ ¬p) ∧ (¬q ∨ r)) ∧ q))
+                                (s7: ((¬p ∨ r) ∧ (¬q ∨ ¬p) ∧ (¬q ∨ r)) ∧ q ↔ (¬p ∨ r) ∧ (¬q ∨ r) ∧ (¬q ∨ ¬p) ∧ q))
+                            (s8: (¬p ∨ r) ∧ (¬q ∨ r) ∧ (¬q ∨ ¬p) ∧ q ↔ (¬(p ∨ q) ∨ r) ∧ ((¬q ∧ q) ∨ (¬p ∧ q))))
+                        (s9 : (¬(p ∨ q) ∨ r) ∧ ((¬q ∧ q) ∨ (¬p ∧ q)) ↔ (¬(p ∨ q) ∨ r) ∧ (¬p ∧ q)))
+                    (s11 : (¬(p ∨ q) ∨ r) ∧ (¬p ∧ q) ↔ (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q))))
+                (s12 : (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q)) ↔ (r ∧ (¬p ∧ q)) ∨ (¬p ∧ ¬q ∧ ¬p ∧ q)))
+            (s13 : (r ∧ (¬p ∧ q)) ∨ (¬p ∧ ¬q ∧ ¬p ∧ q) ↔ ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q))
+        (s14 : ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q ↔ ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q))
+    (s15: ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ ((¬p ∧ q) ∧ r)))
+
+check assessedExercice1
+
 -- Informally we have to show that:
 -- ¬(p ∨ (q ∧ ¬r)) ∧ q ↔
 -- (¬p ∧ q) ∧ r
@@ -132,14 +207,15 @@ lemma s2 : (¬p ∧ ¬(q ∧ ¬r)) ∧ q → (¬p ∧ (¬q ∨ r)) ∧ q :=
 -- ¬(p ∨ (q ∧ ¬r)) ∧ q ↔ (s1)
 -- (¬p ∧ ¬(q ∧ ¬r)) ∧ q ↔ (s2)
 -- ¬p ∧ (¬q ∨ r) ∧ q ↔ (s3)
--- ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q ↔
--- ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔
--- (((¬p ∨ ¬p) ∧ (¬p ∨ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔
--- ((¬p ∨ r) ∧ (¬q ∨ ¬p) ∧ (¬q ∨ r)) ∧ q ↔
--- (¬p ∨ r) ∧ (¬q ∨ r) ∧ (¬q ∨ ¬p) ∧ q ↔
--- (¬(p ∨ q) ∨ r) ∧ ((¬q ∧ q) ∨ (¬p ∧ q)) ↔
--- (¬(p ∨ q) ∨ r) ∧ (¬p ∧ q) ↔
--- (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q)) ↔
--- (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q)) ↔
--- (r ∧ (¬p ∧ q)) ∨ (¬p ∧ ¬q ∧ ¬p ∧ q) ↔
--- (r ∧ (¬p ∧ q)) ∨ 0
+-- ((¬p ∧ ¬q) ∨ (¬p ∧ r)) ∧ q ↔ (s4)
+-- ((¬p ∨ (¬p ∧ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ (s5)
+-- (((¬p ∨ ¬p) ∧ (¬p ∨ r)) ∧ (¬q ∨ (¬p ∧ r))) ∧ q ↔ (s6)
+-- ((¬p ∨ r) ∧ (¬q ∨ ¬p) ∧ (¬q ∨ r)) ∧ q ↔ (s7)
+-- (¬p ∨ r) ∧ (¬q ∨ r) ∧ (¬q ∨ ¬p) ∧ q ↔ (s8)
+-- (¬(p ∨ q) ∨ r) ∧ ((¬q ∧ q) ∨ (¬p ∧ q)) ↔ (s9)
+-- (¬(p ∨ q) ∨ r) ∧ (¬p ∧ q) ↔ (s10)
+-- (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q)) ↔ (s11)
+-- (r ∧ (¬p ∧ q)) ∨ (¬(p ∨ q) ∧ (¬p ∧ q)) ↔ (s12)
+-- (r ∧ (¬p ∧ q)) ∨ (¬p ∧ ¬q ∧ ¬p ∧ q) ↔ (s13)
+-- (r ∧ (¬p ∧ q)) ∨ 0 ↔ (s14)
+-- ((¬p ∧ q) ∧ r)
