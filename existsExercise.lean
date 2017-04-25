048802ab5cf30d98035fcde2186a609bc7ffe503
picrin <Adam Kurkiewicz>
2017-04-25 17:04:24 +0100

    partially migrating to lean 3

--- existsExercise.lean
+++ existsExercise.lean
@@ -8,10 +8,10 @@ lemma doubleNegate {r : Prop} : r → ¬ ¬ r := assume (Hr : r) (nHr : ¬ r), n
 
 lemma doubleNegateRev {r : Prop} : ¬ ¬ r → r :=
     assume (Hr : ¬ ¬ r),
-        have r ∨ ¬ r, from em r,
-        or.elim `r ∨ ¬r` (λ (H : r), H) (λ (H : ¬ r), false.elim (Hr H))
+        have J : r ∨ ¬ r, from em r,
+        or.elim J (λ (H : r), H) (λ (H : ¬ r), false.elim (Hr H))
 
-lemma compose {p q r : Prop} : (p → q) → (q → r) → (p → r) := 
+lemma compose {p q r : Prop} : (p → q) → (q → r) → (p → r) :=
     λ (H : (p → q)),
         λ (H1 : (q → r)),
             λ (Hp : p), H1 (H Hp)
@@ -20,12 +20,12 @@ lemma compose {p q r : Prop} : (p → q) → (q → r) → (p → r) :=
 lemma existsNotForall : (∃ x, p x) → ¬ (∀ x, ¬ p x) :=
     assume Hexists : (∃ x, p x),
     assume Hforall : ((∀ x, ¬ p x)),
-    show false, from obtain (x : A) (P : p x ), from Hexists, Hforall x P
+    exists.elim Hexists (assume a : A, Hforall a)
 
 lemma existsFalseNotForall : (∃ x, ¬ p x) → ¬ (∀ x, p x) :=
     assume H : (∃ x, ¬ p x),
     assume H1 : (∀ x, p x),
-    have H1Rewr : (∀ x, ¬ ¬ p x), from (λ (x : A), doubleNegate (H1 x)), 
+    have H1Rewr : (∀ x, ¬ ¬ p x), from (λ (x : A), doubleNegate (H1 x)),
     have flema : (∃ x, ¬ p x) → ¬ (∀ x, ¬ ¬ p x), from (existsNotForall A (λ (a : A), ¬ p a)),
     have notForall : ¬ (∀ x, ¬ ¬ p x), from flema H,
     show false, from notForall H1Rewr
@@ -57,22 +57,27 @@ theorem distributiveForAll : (∀ (x : A), r ∨ p x) ↔ (r ∨ ∀ (x : A), p 
             (λ (PApx : (∀ (x : A), p x)), (λ (x : A), or.intro_right (r) (PApx x))))
 
 
-example : (∃ x : A, r) → r := 
+example : (∃ x : A, r) → r :=
     assume S1 : (∃ x : A, r),
     have S2 : (A → r → r), from (λ a : A, λ Hr : r, Hr),
     (exists.elim S1) S2
-    
+
 example : r → (∃ x : A, r) := λ (Hr : r), exists.intro a Hr
 
 example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := iff.intro
-    (λ (HexistTogether : (∃ x, p x ∧ r)),
-        obtain (x : A) (Hpxr : p x ∧ r), from HexistTogether, 
-        and.intro (exists.intro x (and.elim_left Hpxr)) (and.elim_right Hpxr))
-    (λ (HexistSeparately : (∃ x, p x) ∧ r),
-        obtain (x : A) (Hpx : p x), from and.elim_left HexistSeparately,
-        exists.intro x (and.intro Hpx (and.elim_right HexistSeparately)))
+    (assume (HexistsTogether : (∃ x, p x ∧ r)),
+    exists.elim HexistsTogether
+        (assume a : A,
+            (assume H : p a ∧ r,
+                and.intro
+                    (exists.intro a (and.elim_left H))
+                    (and.elim_right H))))
+    (assume (HexistSeparately : (∃ x, p x) ∧ r),
+            exists.elim (and.elim_left HexistSeparately)
+                (assume (x : A), assume (Hpx : p x),
+                    exists.intro x (and.intro Hpx (and.elim_right HexistSeparately))))
 
-example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := 
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) :=
     iff.intro
         (assume (H : (∃ x, p x ∨ q x)),
             obtain (x : A) (Por : p x ∨ q x), from H, (or.elim Por)
@@ -87,7 +92,7 @@ example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) :=
                     obtain (x : A) (Hqx : q x), from right,
                         exists.intro x (or.intro_right (p x) Hqx)))
 
-example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := 
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) :=
     iff.intro
     (assume (H : ∀ x, p x),
         (assume (H1 : (∃ x, ¬ p x)),
@@ -99,7 +104,7 @@ example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) :=
             (λ (H1 : ¬ p x), (show false, from
                 have H2 : (∃ x, ¬ p x), from exists.intro x H1, H H2))))
 
-lemma existsEqNotForall : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := 
+lemma existsEqNotForall : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) :=
     iff.intro
     (assume H : (∃ x, p x),
         (λ (H1 : (∀ x, ¬ p x)),
@@ -114,12 +119,12 @@ lemma existsEqNotForall : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) :=
             from (iff.elim_left (distributiveForAll A (λ (x : A), ¬ p x) (∃ y, p y))) S2,
             show (∃ y, p y), from or.elim S3 (λ (Hexists : (∃ y, p y)), Hexists) (λ (HforallNot : (∀ x, ¬ p x)), false.elim(H HforallNot)))
 
-lemma contrapositive {p : Prop} {q : Prop} : (p → q) → (¬ q → ¬p) := 
+lemma contrapositive {p : Prop} {q : Prop} : (p → q) → (¬ q → ¬p) :=
     assume (H : (p → q)),
         assume (S1 : ¬ q),
             assume (S2 : p), S1 (H S2)
 
-lemma iffContr {p : Prop} {q : Prop} : (p ↔ q) → (¬ p ↔ ¬ q) := 
+lemma iffContr {p : Prop} {q : Prop} : (p ↔ q) → (¬ p ↔ ¬ q) :=
     (λ (H : p ↔ q),
         have p → q, from iff.elim_left H,
         have q → p, from iff.elim_right H,
@@ -136,25 +141,25 @@ example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) :=
                 (λ (H : (∀ x, ¬ p x)), doubleNegate H)
                 (S2 : ¬ ¬ (∀ x, ¬ p x) → ¬ (∃ x, p x)))
 
-example : (∀ x, r → p x) → (r → ∀ x, p x) := 
+example : (∀ x, r → p x) → (r → ∀ x, p x) :=
     (λ (H : (∀ x, r → p x)), λ (Pr : r), λ (x : A), H x Pr)
 
-example : (r → ∀ x, p x) → (∀ x, r → p x) := 
+example : (r → ∀ x, p x) → (∀ x, r → p x) :=
     (λ (H : (r → ∀ x, p x)), λ (x : A), λ (Pr : r), (H Pr) x)
 
 example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := iff.intro (notForallExists A p) (existsFalseNotForall A p)
 
-example : (∀ x, p x → r) → (∃ x, p x) → r := 
+example : (∀ x, p x → r) → (∃ x, p x) → r :=
     assume (H : (∀ x, p x → r)), λ (H1 : (∃ x, p x)),
         obtain (x : A) (H2 : p x), from H1, H x H2
 
-example : ((∃ x, p x) → r) → (∀ x, p x → r) := 
+example : ((∃ x, p x) → r) → (∀ x, p x → r) :=
     assume (H : (∃ x, p x) → r),
         λ (x : A),
             λ (Ppx : p x),
                 H (exists.intro x Ppx)
 
-example : (∃ x, p x → r) → (∀ x, p x) → r := 
+example : (∃ x, p x → r) → (∀ x, p x) → r :=
     λ (H : (∃ x, p x → r)),
         λ (H1 : (∀ x, p x)),
             obtain (x : A) (Hpxr : p x → r), from H,
